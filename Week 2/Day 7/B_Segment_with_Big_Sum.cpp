3fdcebb9e9dd3a194eba66a502ceaafdd95a9c4a
Monarul Islam <monarul007@gmail.com>
2024-03-28 23:31:51 +0600

    Week 2 Day 7

---
+++ Week 2/Day 7/B_Segment_with_Big_Sum.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // Write your code here
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+    long long n, s;
+    cin >> n >> s;
+
+    long long arr[n];
+    for (long long i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    long long minLength = n;
+    long long left = 0, right = 0, sum = 0;
+    while (right < n) {
+        sum += arr[right];
+        while (sum >= s && left <= right) {
+            minLength = min(minLength, right - left + 1);
+            sum -= arr[left];
+            left++;
+        }
+        right++;
+    }
+
+    if (minLength == n) {
+        cout << -1;
+    } else {
+        cout << minLength;
+    }
+    
+    return 0;
+}
