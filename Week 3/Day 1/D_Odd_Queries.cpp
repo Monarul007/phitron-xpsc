8d4d6069e223bfd56d12e8f14f5d6e94713e2e5d
Monarul Islam <monarul007@gmail.com>
2024-03-30 17:24:50 -0700

    Week 3 Day 2

--- Week 3/Day 1/D_Odd_Queries.cpp
+++ Week 3/Day 1/D_Odd_Queries.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long n, a[200005], q, sum=0, pref[200005];
+long long n, arr[200005], q, sum=0, pref[200005];
 int main()
 {
     // Write your code here
@@ -13,21 +13,20 @@ int main()
 
         sum = 0;
         cin >> n >> q;
-        for(int i=1;i<=n;i++){
-            cin >> a[i];
-            sum+=a[i];
-            pref[i]=pref[i-1];
-            pref[i]+=a[i];
+        for(int i= 1; i <= n; i++){
+            cin >> arr[i];
+            sum += arr[i];
+            pref[i] = pref[i - 1] + arr[i];
         }
 
         for(int i = 0; i < q; i++){
-            long long l,r,k;
+            long long l, r, k;
             cin >> l >> r >> k;
-            long long ans = pref[n]-(pref[r]-pref[l-1])+k*(r-l+1);
-            if(ans%2==1){
-                cout<<"YES"<<endl;
+            long long ans = pref[n] - (pref[r] - pref[l - 1]) + k * (r - l + 1);
+            if(ans % 2 == 1){
+                cout << "YES" << endl;
             }else{
-                cout<<"NO"<<endl;
+                cout << "NO" << endl;
             }
         }
     }
